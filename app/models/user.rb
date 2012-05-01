8d3b9b261f18d991c39abe74a3718831526d8b71
Kindy Tice <kindy.tice@gmail.com>
2012-05-01 00:21:07 -0700

    protofeed, 0 errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,19 +16,20 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence:   true,
-            format:     { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
+  validates :email, presence: true,
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+  validates :password, length: {minimum: 6}
   validates :password_confirmation, presence: true
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 end
 
-def feed
-  # This is preliminary. See "Following users" for the full implementation.
-  Micropost.where("user_id = ?", id)
-end
 
 private
 
