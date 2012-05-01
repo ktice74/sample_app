28b70a77b5c0317d495956aea2d4068f9a19a263
Kindy Tice <kindy.tice@gmail.com>
2012-05-01 02:59:25 -0700

    ajax, 2 errors

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -81,6 +81,17 @@ describe "Authentication" do
               page.should have_selector('title', text: user.name)
             end
           end
+          describe "in the Relationships controller" do
+            describe "submitting to the create action" do
+              before { post relationships_path }
+              specify { response.should redirect_to(signin_path) }
+            end
+
+            describe "submitting to the destroy action" do
+              before { delete relationship_path(1) }
+              specify { response.should redirect_to(signin_path) }
+            end
+          end
         end
       end
 
